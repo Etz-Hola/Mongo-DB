49f66bf594410063d14cae059dbf1d06201bd0bb
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-18 11:01:56 +0100

    u

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -33,17 +33,20 @@ const updateEmployee = async (req, res) => {
     if (req.body?.firstname) employee.firstname = req.body.firstname;
     if (req.body?.lastname) employee.lastname = req.body.lastname;
     const result = await employee.save();
+    res.json(result);
     
 }
 
-const deleteEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const deleteEmployee = async(req, res) => {
+    if (!req?.body?.id) return res.status(400).json({'message': 'Employee ID required'});
+
+    const employee = await employee.findOne({_id: req.body.id}).exec()
     if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+        return res.status(204).json()({'message': `No employee matches ID ${req.body.id}`})
     }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
+    const result = await employee.deleteOne();
+    result.json(result);
+    
 }
 
 const getEmployee = (req, res) => {
